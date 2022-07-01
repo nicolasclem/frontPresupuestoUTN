dae0770396edb8cc9cdaa3e290a4959d9b36d447
Nicolas Clementi <clementinicolas@gmail.com>
2022-07-01 17:15:11 -0300

    cambios en card info

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -53,9 +53,7 @@ const CardInfo = () => {
     />
     <CardContent>
       <Typography variant="body2" color="text.secondary">
-        This impressive paella is a perfect party dish and a fun meal to cook
-        together with your guests. Add 1 cup of frozen peas along with the mussels,
-        if you like.
+     INGRESOS
       </Typography>
     </CardContent>
     <CardActions disableSpacing>
@@ -70,10 +68,9 @@ const CardInfo = () => {
     </CardActions>
     <Collapse in={expanded} timeout="auto" unmountOnExit>
       <CardContent>
-        <Typography paragraph>Method:</Typography>
+        <Typography paragraph>Semanales:</Typography>
         <Typography paragraph>
-          Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-          aside for 10 minutes.
+          Ingresos
         </Typography>
         <Typography paragraph>
           Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
@@ -83,17 +80,6 @@ const CardInfo = () => {
           pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
           stirring often until thickened and fragrant, about 10 minutes. Add
           saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-        </Typography>
-        <Typography paragraph>
-          Add rice and stir very gently to distribute. Top with artichokes and
-          peppers, and cook without stirring, until most of the liquid is absorbed,
-          15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-          mussels, tucking them down into the rice, and cook again without
-          stirring, until mussels have opened and rice is just tender, 5 to 7
-          minutes more. (Discard any mussels that don&apos;t open.)
-        </Typography>
-        <Typography>
-          Set aside off of the heat to let rest for 10 minutes, and then serve.
         </Typography>
       </CardContent>
     </Collapse>
